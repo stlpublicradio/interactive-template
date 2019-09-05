c8446808190320f938fe5f744525b14a41d0ab51
Brent Jones <bjones@bjones.net>
2019-09-05 10:07:19 -0500

    closes #1

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ var exec = require("child_process").exec;
 var path = require("path");
 
 exports.description = "A standard starting-point for news app development at NPR."
-exports.template = function(grunt, init, done) {
+exports.template = function (grunt, init, done) {
   //prelims
   var here = path.basename(process.cwd());
 
@@ -11,17 +11,19 @@ exports.template = function(grunt, init, done) {
     init.prompt("author_name"),
     init.prompt("app_name", here),
     init.prompt("app_description"),
-    init.prompt("github_repo", "nprapps/" + here)
-  ], function(err, props) {
+    init.prompt("github_repo", "stlpublicradio/" + here)
+  ], function (err, props) {
     //add environment variables, dynamic properties
-    
+
     var root = init.filesToCopy(props);
-    init.copyAndProcess(root, props, { noProcess: "src/assets/**" });
+    init.copyAndProcess(root, props, {
+      noProcess: "src/assets/**"
+    });
     grunt.file.mkdir("data");
 
     //install node modules
     console.log("Installing Node modules...");
-    exec("npm install --cache-min 999999", function(err, stdin, stderr) {
+    exec("npm install --cache-min 999999", function (err, stdin, stderr) {
       if (err) console.error(stdin, stderr);
       done();
     });
